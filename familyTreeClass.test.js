cd321c5bdb96dff9daff5927766f79dc3da93c45
AbdiArr <abdirisak.ahmed@sky.uk>
2021-10-13 17:48:08 +0100

    renamed file

---
+++ familyTreeClass.test.js
@@ -0,0 +1,56 @@
+const { expect, test } = require("@jest/globals");
+const { royalFamilyTree, Person } = require("./familyTreeClass");
+
+describe("Royal Family Tree Object and Person Class Tests", () => {
+  test.skip("Failing Test: Testing if royalFamilyTree is an instance of Person", () => {
+    expect(royalFamilyTree instanceof Person).toBe(true);
+  });
+
+  test.skip("Failing Test: Testing if royalFamilyTree is an instance of Person", () => {
+    expect(royalFamilyTree instanceof Person).toBe(true);
+  });
+
+  test.skip("Failing test: Testing if type of royalFamilyTree is 'string'", () => {
+    expect(typeof royalFamilyTree).toBe("string");
+  });
+
+  test.skip("Passing test: Testing if type of royalFamilyTree is 'object'", () => {
+    expect(typeof royalFamilyTree).toBe("object");
+  });
+
+  test.skip("Testing third generation childOf methods returning expected parents", () => {
+    expect(royalFamilyTree.thirdGeneration[0].childOf()).toBe("unknown");
+    expect(royalFamilyTree.thirdGeneration[1].childOf()).toBe(
+      "Prince Philip, Duke of Edinburgh & Queen Elizabeth II"
+    );
+    expect(royalFamilyTree.thirdGeneration[2].childOf()).toBe("unknown");
+    expect(royalFamilyTree.thirdGeneration[3].childOf()).toBe(
+      "Prince Philip, Duke of Edinburgh & Queen Elizabeth II"
+    );
+    expect(royalFamilyTree.thirdGeneration[4].childOf()).toBe(
+      "Prince Philip, Duke of Edinburgh & Queen Elizabeth II"
+    );
+    expect(royalFamilyTree.thirdGeneration[5].childOf()).toBe(
+      "Prince Philip, Duke of Edinburgh & Queen Elizabeth II"
+    );
+  });
+
+  test.skip("Testing second generation childOf methods returning expected parents", () => {
+    expect(royalFamilyTree.secondGeneration[0].childOf()).toBe("unknown");
+    expect(royalFamilyTree.secondGeneration[1].childOf()).toBe(
+      "King George VI & Queen Elizabeth, The Queen Mother"
+    );
+    expect(royalFamilyTree.secondGeneration[2].childOf()).toBe(
+      "King George VI & Queen Elizabeth, The Queen Mother"
+    );
+  });
+
+  test.skip("Testing first generation childOf methods returning expected parents", () => {
+    expect(royalFamilyTree.firstGeneration[0].childOf()).toBe("unknown");
+    expect(royalFamilyTree.firstGeneration[1].childOf()).toBe("unknown");
+  });
+
+  test("Test Driven Development Cycle (TDD) example: Red, Green, Refactor.", () => {
+    expect(royalFamilyTree.firstGeneration[0].sayHi()).toBe("Hello");
+  });
+});
